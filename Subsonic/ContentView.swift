488b59a62351aa353bd0ac35548493beb4925c2f
Bilal Benlarbi <>
2020-05-31 19:22:24 +0200

    Initial commit

---
+++ Subsonic/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Subsonic
+//
+//  Created by Bilal on 31/05/2020.
+//  Copyright © 2020 Bilal Benlarbi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
