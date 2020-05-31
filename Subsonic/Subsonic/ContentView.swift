691cef5888d4990adfeb60cf8462194dd9ef8f40
Bilal Benlarbi <>
2020-05-31 20:05:31 +0200

    SwiftLint configured

--- Subsonic/Subsonic/ContentView.swift
+++ Subsonic/Subsonic/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var selection = 0
  
     var body: some View {
-        TabView(selection: $selection){
+        TabView(selection: $selection) {
             Text("First View")
                 .font(.title)
                 .tabItem {
