9bbabbe00d0108f8b1eb6b3a26663954c7939947
Bilal Benlarbi <>
2020-05-31 19:54:18 +0200

    SubsonicKit added

---
+++ SubsonicKit/SubsonicKit/SubsonicKit.h
@@ -0,0 +1,19 @@
+//
+//  SubsonicKit.h
+//  SubsonicKit
+//
+//  Created by Bilal on 31/05/2020.
+//  Copyright © 2020 Bilal Benlarbi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubsonicKit.
+FOUNDATION_EXPORT double SubsonicKitVersionNumber;
+
+//! Project version string for SubsonicKit.
+FOUNDATION_EXPORT const unsigned char SubsonicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubsonicKit/PublicHeader.h>
+
+
